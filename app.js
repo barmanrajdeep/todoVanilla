fc9258e61b47f2a5df03d3e9ae43273665298253
soumyadeep <soumyadeepbarman1996@gmail.com>
2019-05-01 17:42:54 +0530

    complete functionality

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ function todoList () {
     var item = document.getElementById("todoInput").value
     var text = document.createTextNode(item)
     var newItem = document.createElement("li")
-    newItem.appendChild(text)
+    var checkBox = document.createElement("input")
+    checkBox.type ="checkbox";
+    newItem.append(text)
+    newItem.prepend(checkBox)
     document.getElementById("todoList").appendChild(newItem)
+}
+function selectedDone () {
+    // get all the list item
+    //loop over each is it has checked
+    //if it's checked then it should be append in doneList
+    var doneList = document.getElementById("DoneLIst")
+    var items = document.getElementById("todoList").childNodes
+    for (i=0 ; i< items.length ;i++) {
+     var listedItem =items[i];
+     for (var j=0 ; j<listedItem.children.length ; j++){
+        if(
+            listedItem.children[j].tagName=="INPUT"
+        ){
+            if(listedItem.children[j].checked){
+                //appended to doneList
+                doneList.append(listedItem)
+            }
+            
+        }
+     }
+    }
+    
+
 }
