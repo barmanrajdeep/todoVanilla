4f4cbecdab8627022e83b29ca0f98b13f7b95a54
Rajdeep Barman <barman.rajdeep@gmail.com>
2019-05-01 18:50:50 +0530

    clean the code

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 function todoList () {
-    var item = document.getElementById("todoInput").value
-    var text = document.createTextNode(item)
-    var newItem = document.createElement("li")
-    var checkBox = document.createElement("input")
+    var item = document.getElementById("todoInput").value;
+    var text = document.createTextNode(item);
+    var newItem = document.createElement("li");
+    var checkBox = document.createElement("input");
     checkBox.type ="checkbox";
     newItem.append(text)
-    newItem.prepend(checkBox)
+    newItem.prepend(checkBox);
     document.getElementById("todoList").appendChild(newItem)
 }
 function selectedDone () {
-    // get all the list item
-    //loop over each is it has checked
-    //if it's checked then it should be append in doneList
-    var doneList = document.getElementById("DoneLIst")
-    var items = document.getElementById("todoList").childNodes
-    for (i=0 ; i< items.length ;i++) {
+    var doneList = document.getElementById("DoneLIst");
+    var items = document.getElementById("todoList").childNodes;
+    for (var i = 0 ; i< items.length ;i++) {
      var listedItem =items[i];
-     for (var j=0 ; j<listedItem.children.length ; j++){
-        if(
-            listedItem.children[j].tagName=="INPUT"
-        ){
+     for (var j = 0 ; j<listedItem.children.length ; j++) {
+        if(listedItem.children[j].tagName==="INPUT"){
             if(listedItem.children[j].checked){
                 //appended to doneList
                 doneList.append(listedItem)
